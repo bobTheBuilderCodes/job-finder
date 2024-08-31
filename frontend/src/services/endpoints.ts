5e38b5759552ae0bfd2c9a3b85dd5c57c241f096
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2024-08-31 16:32:36 +0000

    Update endpoint
    Hosted backend endpoint to make it accessible to everyone

--- frontend/src/services/endpoints.ts
+++ frontend/src/services/endpoints.ts
@@ -1,5 +1,5 @@
 // export const BASEURL = "http://localhost:5001/api/v1/"
-export const BASEURL = "https://backend-bzbhya9qd-bobthebuildercodes-projects.vercel.app/api/v1/"
+export const BASEURL = "https://backend-bay-ten-31.vercel.app/api/v1/"
 export const JobsAPI = {
     getJobs : `jobs`,
 
